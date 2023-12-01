8ba951975c6c68dc984f78376f9d7d7c105dcb43
Zhangicoder <151625047+Zhangicoder@users.noreply.github.com>
2023-12-01 08:27:55 +0800

    Add files via upload

--- test_2023_11_30.cpp
+++ test_2023_11_30.cpp
@@ -2,22 +2,40 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include<stdio.h>
 #include<stdlib.h>
+#include<string.h>
+#include<assert.h>
+//size_t my_strlen(const char* str)
+//{
+//	size_t count = 0;
+//	while (*str != '\0')
+//	{
+//		count++;
+//		str++;
+//	}
+//	return count;
+//}
+//int main()
+//{
+//	char arr[] = "abcdef";
+//	size_t n = my_strlen(arr);
+//	printf("%u\n", n);
+//	return 0;
+//}
+void my_strcpy(char* p2,const char* p1)
+{
+	assert(p2);
+	assert(p1);
+	while (*p1)
+	{
+		*p2++ = *p1++;
+	}
+	*p2 = *p1;
+}
 int main()
 {
-	int n, k;
-	for (; scanf("%d %d", &n, &k) != EOF;);
-	{	
-		int a[n];
-		for (int j = 1; j <= k; j++)
-		{
-			for (int i = 0; i < n; i++)
-			{
-				if ((i + 1) % j == 0)
-				{
-					a[i] = 1 - a[i];
-				}
-			}
-		}
-	}    
+	char arr1[] = "abcdef";
+	char arr2[20] = { 0 };
+	my_strcpy(arr2, arr1);
+	printf("%s\n", arr2);
 	return 0;
 }
